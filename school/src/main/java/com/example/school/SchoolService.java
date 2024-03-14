660d2cf5d4fa87a4931ae6f54bb74e5700aaf518
kitou88C39 <kanfutrooper@icloud.com>
2024-03-14 16:21:19 +0900

    SchoolService.javaを作成する

--- school/src/main/java/com/example/school/SchoolService.java
+++ school/src/main/java/com/example/school/SchoolService.java
@@ -1,3 +1,21 @@
+package com.alibou.school;
+
+import lombok.RequiredArgsConstrucutor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstrucutor
 public class SchoolService {
+
+    private final SchoolRepository repository;
+
+    public void saveStudent(School school) {
+        repository.save(school);
+
+    }
+
+    public List<School> findAllSchools(){
+        return repository.findAll()
+    }
 
 }
