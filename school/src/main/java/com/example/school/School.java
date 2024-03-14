d489ab6d6f80a9b144810cb5069fdab924237b9e
kitou88C39 <kanfutrooper@icloud.com>
2024-03-14 15:28:58 +0900

    School.javaに作成及び貼り付けて編集する

---
+++ school/src/main/java/com/example/school/School.java
@@ -0,0 +1,20 @@
+package com.alibou.school;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@setter
+@AllArgesConstructor
+@NoArgesConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer schoolId;
+}
