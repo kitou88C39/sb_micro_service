99802feb937422f34697848cf0d3322b16b42f89
kitou88C39 <kanfutrooper@icloud.com>
2024-03-14 16:28:52 +0900

    SchoolService.javaを作成する

--- school/src/main/java/com/example/school/SchoolController.java
+++ school/src/main/java/com/example/school/SchoolController.java
@@ -1,3 +1,35 @@
-public class SchoolController {
+ackage com.alibou.student;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private StudentService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStaus.CREATED)
+    public void save(
+            @RequestBody Student student) {
+
+        service.saveStudent(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok(service.findAllStudents());
+    }
 
 }
