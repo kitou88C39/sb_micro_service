9e073d321199921a7789157c12efd4204457bd18
kitou88C39 <kanfutrooper@icloud.com>
2024-03-14 14:24:56 +0900

    StudentController.javaを作成する

--- student/src/main/java/com/alibou/student/StudentController.java
+++ student/src/main/java/com/alibou/student/StudentController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
@@ -20,5 +24,12 @@ public class StudentController {
     public void save(
             @RequestBody Student student) {
 
+        service.saveStudent(student);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok(service.findAllStudents());
+    }
+
 }
