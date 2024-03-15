f463c0ca7bae5b25608747b2fc50ac1fbfd6c07b
kitou88C39 <kanfutrooper@icloud.com>
2024-03-15 17:42:56 +0900

    Student.javaを作成する

--- school/src/main/java/com/example/school/FullSchollResponse.java
+++ school/src/main/java/com/example/school/FullSchollResponse.java
@@ -1,9 +1,17 @@
 package com.example.school;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 public class FullSchollResponse {
 
     private String name;
 
     private String email;
+
+    List<Student> students;
 
 }
