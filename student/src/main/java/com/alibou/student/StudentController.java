966ab7555aa4eb5ddf2b13c420ef84c7becd88d7
kitou88C39 <kanfutrooper@icloud.com>
2024-03-17 21:22:54 +0900

    StudentController.javaを編集する

--- student/src/main/java/com/alibou/student/StudentController.java
+++ student/src/main/java/com/alibou/student/StudentController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -32,8 +33,9 @@ public class StudentController {
         return ResponseEntity.ok(service.findAllStudents());
     }
 
-    @GetMapping
-    public ResponseEntity<List<Student>> findAllStudents() {
+    @GetMapping("/school/{school-id}")
+    public ResponseEntity<List<Student>> findAllStudents(
+            @PathVariable("school-id") Integer schoolId) {
         return ResponseEntity.ok(service.findAllStudents());
     }
 
