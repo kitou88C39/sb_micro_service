646f6d0de777b0305c96e8bddb32dc1c88e0bf42
kitou88C39 <kanfutrooper@icloud.com>
2024-03-18 16:28:41 +0900

    StudentClient.javaを編集する

--- school/src/main/java/com/example/client/StudentClient.java
+++ school/src/main/java/com/example/client/StudentClient.java
@@ -3,11 +3,11 @@ package com.example.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 @FeignClient(name = "student-service", url = "${application.config.students-url}")
 public interface StudentClient {
 
     @GetMapping("/school/{school-id}")
-    List<Student>fundAllStudentsBySchool(@PathVariable)
-
+    List<Student> fundAllStudentsBySchool(@PathVariable("school-id") Integer schoolId);
 }
