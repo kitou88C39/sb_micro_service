086a6917841c626df5b6b992572b9031915e9185
kitou88C39 <kanfutrooper@icloud.com>
2024-03-19 20:58:57 +0900

    ConfigServerApplication.javaを編集する

--- config-server/src/main/java/com/alibou/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/alibou/configserver/ConfigServerApplication.java
@@ -3,11 +3,11 @@ package com.alibou.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnabaleConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
